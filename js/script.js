4ac00f8aaf6bd5e9c73a52937c5faad4cc4df61e
drashti <riddhithekdi11111@gmail.com>
2021-09-03 18:06:51 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
-$(window).on("load",function(){
-     $("#loader").delay(1000).fadeOut(1000);
+$(window).on("load", function() {
+    $("#loader").delay(1000).fadeOut(1000);
 });
 
 
-$('.owl-carousel').owlCarousel({
+// $('.banner-slider').owlCarousel({
+//     loop: true,
+//     items: 1,
+//     dots: true,
+//     nav: false,
+//     smartSpeed: 900,
+//     autoplay: true,
+//     responsiveClass: true,
+
+
+// });
+$('.banner-slider').owlCarousel({
+    loop: true,
+    margin: 50,
+    dots: true,
+    nav:false,
+    smartSpeed:900,
+    autoplay:true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
+
+
+$('.owl-two').owlCarousel({
     loop:true,
     items:1,
     dots:true,
@@ -15,10 +47,11 @@ $('.owl-carousel').owlCarousel({
   
 });
 
-function onclickmenu(){
+function onclickmenu() {
     document.getElementById("menu").classList.toggle("greenlist");
     document.getElementById("list").classList.toggle("change");
-   
-   
 }
+
+ 
+
 
